ea26169f5a3fac4060b61daf58d38a95d52d3cf1
Lautaro <lautaroceballos2013@gmail.com>
2021-12-24 14:01:46 -0300

    Implementacion del componente ModalImage

--- src/componentes/AddEditForm/InputImgField.js
+++ src/componentes/AddEditForm/InputImgField.js
@@ -1,23 +1,9 @@
 import imgDefault from "images/default.jpg"
 import { useDisclosure } from "@chakra-ui/hooks"
-import {
-    Flex,
-    Image,
-    IconButton,
-    Input,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalBody,
-    ModalCloseButton,
-    FormControl, 
-    FormLabel,
-} from "@chakra-ui/react"
+import { Flex, Image, IconButton, Input, FormControl, FormLabel } from "@chakra-ui/react"
 import { AddIcon } from '@chakra-ui/icons'
-
 import { useEffect, useState } from "react"
-// import { useFormikContext } from 'formik'
-import { uploadImage } from "services/firebase"
+import ModalImage from "componentes/ModalImage"
 
 // const IMAGE_STATES = {
 //     ERROR: -1,
@@ -26,45 +12,40 @@ import { uploadImage } from "services/firebase"
 //     COMPLETE: 2
 // }
 
-
-export default function InpuetImgField({values}){
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    // const { setFieldValue } = useFormikContext()
+export default function InpuetImgField({values, setImgFile}){
+    const modalEvent = useDisclosure()
     
-    const [task, setTask] = useState('')
+    const [blobURL, setBlobURL] = useState('')
+    const [imgURL, setImgURL] = useState('')
 
     useEffect(() => {
-        if(task){
-            let onProgress = () => {}
-            let onError = () => {
-                console.log('Error')
-            }
-            let onComplete = () => {
-                console.log('onComplete')
-            }
-
-            task.on('state_changed', onProgress, onError, onComplete)
+        let valuesImg = values.img
+        if(!blobURL && valuesImg.length === 0){
+            setImgURL(imgDefault)
+        } else if (!blobURL && valuesImg.length > 0) {
+            setImgURL(valuesImg)
+        } else {
+            setImgURL(blobURL)
         }
-    }, [task])
+    }, [blobURL, values])
 
     const handleChange = (e) => {
         const file = e.target.files[0]
-        // console.log(file)
-        const task = uploadImage(file, values.name)
-        setTask(task)
-        console.log(task)
-        // setFieldValue('img', file)
+        const blobURL = window.URL.createObjectURL(file)
+        setBlobURL(blobURL)
+        setImgFile(file)  
     }
 
     return <>
         <Flex flexWrap="wrap" width="100%" position="relative">
             <Image
-                onClick={onOpen}
-                src={values.img !== "" ? values.img : imgDefault}
-                alt={values.nombre} width="100%" height={["8em", "15em"]}
+                onClick={modalEvent.onOpen}
+                src={imgURL}
+                alt={values.nombre} 
+                minWidth="100%" 
+                height={["8em", "15em"]}
                 objectFit="cover"
             /> 
-
             <FormControl>
                 <IconButton as={FormLabel} 
                     htmlFor="img"
@@ -78,31 +59,10 @@ export default function InpuetImgField({values}){
                     right="0"
                     cursor="pointer"
                 />
-                <Input 
-                    type="file"
-                    onChange={handleChange}
-                    display="none"
-                    name="img"
-                    id="img"
-                />
+                <Input type="file" onChange={handleChange} display="none" name="img" id="img"/>
             </FormControl>
         </Flex>
         
-
-        <Modal isOpen={isOpen} onClose={onClose} size="xl">
-            <ModalOverlay />
-            <ModalContent bg="transparent">
-                <ModalCloseButton bg="white"/>
-                <ModalBody padding="0">
-                    <Image
-                        onClick={onOpen}
-                        src={values.img !== "" ? values.img : imgDefault}
-                        alt={values.nombre} width="100%" height="100%"
-                        objectFit="cover"
-                        borderRadius="md"
-                    />
-                </ModalBody>
-            </ModalContent>
-        </Modal>
+        <ModalImage src={imgURL} alt={values.name} event={modalEvent}/>
     </>
 }
--- src/componentes/AddEditForm/index.js
+++ src/componentes/AddEditForm/index.js
@@ -1,22 +1,20 @@
 import { Formik, Form } from "formik"
+import { useState } from "react"
 import { addProduct } from "services/firebase.js"
 import InputField from "./InputField"
 import validations from "./validations"
 import InputTagField from "./InputTagField"
-import {
-    Button,
-    Flex,
-    Grid,
-    GridItem
-} from "@chakra-ui/react"
-import InpuetImgField from "./InputImgField"
+import { Button, Flex, Grid, GridItem } from "@chakra-ui/react"
+import InputImgField from "./InputImgField"
 
 export default function AddEditForm() {
+    const [imgFile, setImgFile] = useState('')
+
     return <>
         <Formik
             initialValues={{
                 nombre: '',
-                img: 'https://dam.cocinafacil.com.mx/wp-content/uploads/2021/03/como-hacer-velas-caseras.jpg',
+                img: '',
                 stock: '',
                 costoMayorista: '',
                 bultoCant: '',
@@ -31,8 +29,8 @@ export default function AddEditForm() {
             validate={validations}
 
             onSubmit={(dataForm, { resetForm }) => {
-                console.log(dataForm)
-                addProduct(dataForm)
+
+                addProduct(imgFile, dataForm)
                     .then(() => {
                         resetForm();
                     }).catch((error) => {
@@ -44,8 +42,7 @@ export default function AddEditForm() {
                 <Flex display="flex" flexWrap="wrap" alignContent="start" w={{base: "100%", md: "48em"}}  bg="white" margin="5em auto 0" padding="0 2em" boxShadow="lg">
                     <Form >
                         <Flex display="flex" flexWrap="wrap" alignContent="start" >
-                            <InpuetImgField values={values}/>
-
+                            <InputImgField values={values} setImgFile={setImgFile}/>
                             <Grid templateColumns="repeat(2, 1fr)" gap="0 1em" width="100%">
                                 <GridItem colSpan={2} marginTop="1em">
                                     <InputField name="nombre" label="Nombre" type="text" placeholder="Nombre del Producto" isRequired />
